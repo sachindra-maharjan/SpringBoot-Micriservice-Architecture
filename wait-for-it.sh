07aced63aec2fa9943c0b35184381729f1b8b099
sachindra-maharjan <sachindra.maharjan@outlook.com>
2023-01-31 19:33:59 +0000

    Enabled Config Server.

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,13 +1,12 @@
-# wait-for-it.sh
-#!/bin/bash
+#!/bin/sh
 
 host="$1"
 shift
 cmd="$@"
 
 until curl --output /dev/null --silent --head --fail http://$host; do
-    >&2 echo "Service is unavailable - sleeping"
-    sleep 1
+    >&2 echo "Service http://$host is unavailable - sleeping for 2 seconds"
+    sleep 2
 done
 
 >&2 echo "Service is up - executing command"
